fb66d995beb02c744dfd67bbe63473b0caceb091
Raad <alirezzaraadco@gmail.com>
2025-02-25 01:47:56 +0330

    ADD TelegramBot.py script to the script folders.

---
+++ TelegramBot.py
@@ -0,0 +1,7 @@
+""""
+This Bot is in sync with the other bot when is active and sends notification into telegram bot or channel if you make it admin.
+This Bot automatically READ sql database to see if any new order is added or no. if a new order is added, send it through Telegram.
+"""
+
+import sqlite3 as sql
+import telebot
